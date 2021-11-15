1b61728c7511d6bb1ccd5cc7e33442d36decf73f
JERATHEL JEAN <76412220+jera66@users.noreply.github.com>
2021-11-15 07:02:46 -0500

    bookmarkd-backend complete

---
+++ readme.md
@@ -0,0 +1,68 @@
+###  Introduction
+This is the backend or under-the-hood of a project called bookmark. This project serves as a  backend designed to work with the frontend of the app. It receives requests from the frontend, and forwards these requests back with their respective answers. The server is implemented in Express using Mongoose. 
+
+Bookmark'd  is one of the projects I've been working on at General Assembly. This project was completed in a team of three software engineers and was our first attempt at putting together a full stack web application. 
+
+### Teammates
+- Jerathel
+- Quentin
+- Ras
+ 
+ Live link
+
+### Technologies used
+1- Express/Node
+2- Mongo/Mongoose 
+3- Dotenv 
+4- Cors 
+5- Morgan
+6- React
+
+### List of dependencies
+```js
+require('dotenv').config()
+const {PORT = 3002, DATABASE_URL} = process.env
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+```
+
+
+### Model
+```js
+// The bookmark schema
+const BookmarkSchema = new mongoose.Schema(
+{
+title: String,
+url: String,
+image: String
+},
+{ timestamps: true }
+)
+
+const Bookmark = mongoose.model('Bookmark', BookmarkSchema)
+```
+
+### Middleware
+```Js
+/_ MIDDLEWARE _/
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+```
+
+### Project Backend Overview
+This bookmark project is a three-person group project and each person gets assigned a specific piece of it to work on. This is an app that lets the user add a title and a link to helpful websites. When a title is clicked, it should take the user to the linked website.
+
+### Backend Route Table
+| url             | method | action                       |
+| --------------- | ------ | ---------------------------- |
+| /bookmark/      | get    | get all bookmarks (index)    |
+| /bookmark       | post  | create a new bookmark         |
+| /bookmark/:id   | put    | update a bookmark            |
+| /bookmark/:id   | delete | delete a bookmark            |
+
+### Roadblocks
+One of the major roadblock we encountered was deciding how to divide the project for a three person group, and also handling merge conflicts. I personally cloned a repo inside of another repo and had trouble switching to the dev branch. I took me a while to get it all fixed.
--- server.js
+++ server.js
@@ -1,33 +1,111 @@
-/*          DEPENDENCIES            */
+//////////////////////////////////
+// Dependencies
+/////////////////////////////////
+//Getting .env variables
 require('dotenv').config()
-const {PORT = 3002, DATABASE_URL} = process.env
+//Pulling PORT from .env, giving it a default of 3002 (object destructuring)
+const { PORT = 3002, DATABASE_URL } = process.env
+//Importing express
 const express = require('express')
+//Creating  the application object
 const app = express()
+//Importing mongoose
 const mongoose = require('mongoose')
+//Importing middleware
 const cors = require('cors')
 const morgan = require('morgan')
 
-/*          DATABASE CONNECTION         */
+/////////////////////////////////
+// Database Connection
+////////////////////////////////
+//Establishing connection
 mongoose.connect(DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 })
 
+//Connection Events
 mongoose.connection
-.on('open', () => {console.log('Connected to Mongo')})
-.on('close', () => {console.log('Disconnected from Mongo')})
-.on('error', (error) => {console.log(error)})
+  .on('open', () => console.log('You are connected to Mongo'))
+  .on('close', () => console.log('You are disconnected from Mongo'))
+  .on('error', error => console.log(error))
 
-/*          MIDDLEWARE          */
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
+//////////////////////////////
+// Models
+//////////////////////////////
+// The bookmark  schema
+const BookmarkSchema = new mongoose.Schema(
+  {
+    title: String,
+    url: String,
+    image: String
+  },
+  { timestamps: true }
+)
 
-/*          ROUTES          */
+const Bookmark = mongoose.model('Bookmark', BookmarkSchema)
+
+/////////////////////////////////
+//Middleware
+//////////////////////////////////
+app.use(cors()) //Preventing cors errors, opening up access for frontend
+app.use(morgan('dev')) //Logging
+app.use(express.json()) //Parsing json bodies
+
+////////////////////////////////
+// Routes
+////////////////////////////////
+//Setting up a test route
 app.get('/', (req, res) => {
-    res.send('Hello World')
+  res.send('Hello World!')
 })
 
+//Bookmark index route
+//Getting request to /bookmark links, returning them all as json
+app.get('/bookmark', async (req, res) => {
+  try {
+    //Sending all bookmark links
+    res.json(await Bookmark.find({}))
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+//Bookmark create route
+//Posting request to /bookmark, using request body to make new bookmark links
+app.post('/bookmark', async (req, res) => {
+  try {
+    //Creating a new bookmark link
+    res.json(await Bookmark.create(req.body))
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+//Bookmark update  route
+//Putting request /bookmark/:id, updates bookmark link based on id with request body
+app.put('/bookmark/:id', async (req, res) => {
+  try {
+    //Updating a bookmark link
+    res.json(
+      await Bookmark.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    )
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+// Destroy Route
+//Deleting request to /bookmark/:id, deletes the bookmark link specified
+app.delete('/bookmark/:id', async (req, res) => {
+  try {
+    //Deleting a bookmark link
+    res.json(await Bookmark.findByIdAndRemove(req.params.id))
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
 
-/*          SERVER LISTENER         */
-app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)})
+/////////////////////////////////
+// Server Listener
+/////////////////////////////////
+app.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`)
+})
